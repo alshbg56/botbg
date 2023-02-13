0bf83dd1eb98d1f0076a751b4721fbe19ddf4b93
alshbg56 <124146631+alshbg56@users.noreply.github.com>
2023-02-13 12:27:08 +0300

    Update info.py

--- info.py
+++ info.py
@@ -2,4 +2,4 @@
 
 api_id = 1036626
 api_hash = "2f24c61c519fd5cd1da65f87ffa94de6"
-bot_token = "1866631276:AAE1AHPs_BPX4kfzz8WLW3LsQqqmNe6ShE4"
+bot_token = "1737966707:AAEibvu8gnCEjobLfI5h-d8-71enAbZwhF8"
